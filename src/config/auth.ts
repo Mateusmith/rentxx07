5b2a38e9a5fbdeac49ee8dde05af3c472ecc03dc
mateus <mateus@Smith>
2022-12-16 17:06:33 -0300

    rentxx007

---
+++ src/config/auth.ts
@@ -0,0 +1,9 @@
+export default {
+  secret_token: "e7530085c4473100898ed3ee62ef8",
+  expires_in_token: "15m",
+  secret_refresh_token: "8361d0c7e8234747c503d1bfddb0b13e",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
+
+
